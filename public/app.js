79ec7b9e1d51cee3d8588408282fc5162c8c8e91
chrmod <krzysztof.modras@gmail.com>
2015-06-13 21:36:32 +0200

    not sure what I've just done, but it seems to look better

--- public/app.js
+++ public/app.js
@@ -1,5 +1,14 @@
+function setCrumbs(index) {
+  var $crumbs = $("#crumbs a");
+  $crumbs.removeClass("active");
+  for(var i = 1; i <= index; i++) {
+    $('#crumbs a[data-index='+i+']').addClass("active");
+  }
+}
+
 $(document).ready(function () {
-  $("#crumbs a").click(function () {
-    console.log(this);
+  $("#crumbs a").click(function (el) {
+    var index = $(this).data("index");
+    setCrumbs(index);
   });
 });
