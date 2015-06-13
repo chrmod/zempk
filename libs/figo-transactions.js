400c8d3fb744001c65573d07898b50a41f068e25
chrmod <krzysztof.modras@gmail.com>
2015-06-13 17:19:04 +0200

    figo simplified

--- libs/figo-transactions.js
+++ libs/figo-transactions.js
@@ -2,25 +2,8 @@ var figo = require('figo');
 
 var transactions = function (callback) {
   var session = new figo.Session("ASHWLIkouP2O6_bgA2wWReRhletgWKHYjLqDaqb0LFfamim9RjexTo22ujRIP_cjLiRiSyQXyt2kM1eXU2XLFZQ0Hro15HikJQT_eNeT_9XQ");
-
-  session.get_accounts(function(error, accounts) {
-    if (!error) {
-      accounts.forEach(function(account) {
-          console.log(account.account_number);
-          console.log(account.balance.balance);
-      })
-
-      // Print out the list of all transaction originators/recipients of a specific account.
-      session.get_account("A1.1", function(error, account) {
-        if (!error) {
-          account.get_transactions(null, function(error, transactions) {
-            if (!error) {
-              callback(transactions)
-            }
-          });
-        }
-      });
-    }
+  session.get_transactions(null, function (err, tts) {
+    callback(tts);
   });
 }
 
