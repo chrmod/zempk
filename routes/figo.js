6dfc422195970e731567fa3339c666548d0cae9c
chrmod <krzysztof.modras@gmail.com>
2015-06-13 18:44:00 +0200

    /figo - totals on end of year

--- routes/figo.js
+++ routes/figo.js
@@ -5,18 +5,17 @@ var router = express.Router();
 
 
 function UserData(accounts, trans) {
-  this.current_balance = accounts.map(function (account) {
-    return account.balance.balance;
-  }).reduce(function (prev, curr) {
-    return prev + curr;
-  }, 0);
+
+  var currentDate = new Date();
 
   this.transactions = trans.map(function (t) {
+    var date = new Date(t.booking_date);
+    if(Number(date) > Number(currentDate)) return;
     return {
-      date: new Date(t.booking_date),
+      date: date,
       amount: t.amount
     }
-  });
+  }).filter(function(e){return e});
 
   this.income_by_year = this.transactions.reduce(function (prev, curr) {
     var year = curr.date.getFullYear();
@@ -58,6 +57,17 @@ function UserData(accounts, trans) {
     }
   }, 0);
 
+  this.current_balance = accounts.map(function (account) {
+    return account.balance.balance;
+  }).reduce(function (prev, curr) {
+    return prev + curr;
+  }, 0);
+
+  this.balance_on_end_of_year = {}
+  this.balance_on_end_of_year[2015] = this.current_balance;
+  this.balance_on_end_of_year[2014] = this.balance_on_end_of_year[2015] + this.expenses_by_year[2015] - this.income_by_year[2015];
+  this.balance_on_end_of_year[2013] = this.balance_on_end_of_year[2014] + this.expenses_by_year[2014] - this.income_by_year[2014];
+  this.balance_on_end_of_year[2012] = this.balance_on_end_of_year[2013] + this.expenses_by_year[2013] - this.income_by_year[2013];
 
   this.toString = function ( ) { return JSON.stringify(this) };
 }
