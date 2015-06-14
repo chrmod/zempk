f02bf13bbf54a646c472ff03ff28ffeff7ef7318
chrmod <krzysztof.modras@gmail.com>
2015-06-14 10:57:34 +0200

    Breadcrumbs affect the header

--- public/app.js
+++ public/app.js
@@ -1,13 +1,22 @@
 function setCrumbs(index) {
-  var $crumbs = $("#crumbs a");
+  var text = [
+    "Rookie",
+    "Hustler",
+    "Mogul",
+    "Kingpin"
+  ];
+  var $crumbs = $(".crumbs a");
   $crumbs.removeClass("active");
   for(var i = 1; i <= index; i++) {
-    $('#crumbs a[data-index='+i+']').addClass("active");
+    $('.crumbs a[data-index='+i+']').addClass("active");
   }
+  var iconBar = $(".icons .crumbs a");
+  iconBar.addClass('active');
+  iconBar.text(text[index-1]);
 }
 
 $(document).ready(function () {
-  $("#crumbs a").click(function (el) {
+  $(".crumbs a").click(function (el) {
     var index = $(this).data("index");
     setCrumbs(index);
   });
