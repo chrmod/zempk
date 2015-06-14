2e383a66db8a22895a07db0a994645defdce6751
chrmod <krzysztof.modras@gmail.com>
2015-06-14 13:06:42 +0200

    index shows real transactions

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,33 @@ var express = require('express');
 var router = express.Router();
 var figo = require('../libs/figo-transactions');
 
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {
-    title: 'Hello World!',
-    transactions: [{amount: 20, css: 'green'}, {amount: -23, css: 'red'}]
+  figo.getTransactions(function (tts) {
+    tts = shuffle(tts).slice(tts.length - 15, tts.length);
+    res.render('index', {
+      title: 'Hello World!',
+      transactions: tts
+    });
   });
 });
 
